f4da7e5c21b28e9d1e00841bc9d7396503a7f31b
ironmike84 <michaelgross.tn@gmail.com>
2023-08-25 20:10:28 -0400

    Good

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-let dicks = 500
+let pokemonList = [{
+    Name: "Bulbasaur",
+    Height: 0.7,
+    Types: ["Grass", "Poison"]
+},
+{
+    Name: "Ivysaur",
+    Height: 1,
+    Type: ["Grass", "Poison"]
+},
+{
+    Name: "Venusaur",
+    Height: 2,
+    Type: ["Grass", "Poison"]
+},
+{
+    Name: "Charmander",
+    Height: 0.6,
+    Type: ["Fire"]
+}
+]
