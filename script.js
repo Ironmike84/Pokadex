dffca494e1be875859ccb20e9e2d5ca292ee011f
ironmike84 <michaelgross.tn@gmail.com>
2023-08-26 15:07:27 -0400

    Updated

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let pokemonRepository = (function () {
+
 let pokemonList = [{
     Name: "Bulbasaur",
     Height: 0.7,
-    Types: ["Grass", "Poison"]
+    Type: ["Grass", "Poison"]
 },
 {
     Name: "Ivysaur",
@@ -20,19 +22,64 @@ let pokemonList = [{
 }
 ]
 
-
-function myList(Pokemons){
-    for (let i = 0; i < Pokemons.length; i++){
-     console.log(`${Pokemons[i].Name}, ${Pokemons[i].Height}`)
-     document.write(`Name: ${Pokemons[i].Name}" Height: " ${Pokemons[i].Height}` )
-     
+function add(pokemon){
+    if(
+        typeof pokemon === "object" &&
+        "Name" in pokemon &&
+        "Height" in pokemon &&
+        "Type" in pokemon
+    ){
+        pokemonList.push(pokemon)
+    }else{
+        console.log("Pokemon is not Correct")
+    }
 }
+function getAll(){
+    return pokemonList;
 }
+return {
+    add: add,
+    getAll: getAll,
+}
+})()
 
+console.log(pokemonRepository.getAll())
 
-console.log(myList(pokemonList))
+pokemonRepository.getAll().forEach(function(item){
+    let size;
+    if (item.Height > 1 ){
+        size = 'Wow, Thats a big Pokemon'
+    }else{
+        size = 'Dang, Thats a small Pokemon'
+    }
 
-Object.keys(pokemonList).forEach(function(property){
-console.log(pokemonList[property])
+item.Type.forEach(function(typeItem){
+if (typeItem === "grass"){
+    result = '<span style="color: green"></span>'
+}else if(typeItem == "Fire"){
+    result = '<span style="color: red"></span>'
+}else if(typeItem == "Electric"){
+    result = '<span style="color: yellow"></span>'
+}else if(typeItem == "poison"){
+    result = '<span style="color: rgb(106,42,106)"></span>'
+}else if(typeItem == "psychic"){
+    result = '<span style="color: orange"></span>'
+}
+})
+
+document.write(
+    '<div class="box">' +
+    "Name" +
+    item.Name +
+    "<br>" +
+    "Height: " +
+    item.Height +
+    "m" + 
+    "<br>" +
+    "Type: "+
+    item.Type + 
+    "<br>"+
+    '</div>');
+
 })
 
