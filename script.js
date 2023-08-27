3b89849358fd7cc51cbd2172c9bac80d84bc99f2
ironmike84 <michaelgross.tn@gmail.com>
2023-08-27 19:26:38 -0400

    Updated

--- script.js
+++ script.js
@@ -1,85 +1,87 @@
 let pokemonRepository = (function () {
-
-let pokemonList = [{
-    Name: "Bulbasaur",
-    Height: 0.7,
-    Type: ["Grass", "Poison"]
-},
-{
-    Name: "Ivysaur",
-    Height: 1,
-    Type: ["Grass", "Poison"]
-},
-{
-    Name: "Venusaur",
-    Height: 2,
-    Type: ["Grass", "Poison"]
-},
-{
-    Name: "Charmander",
-    Height: 0.6,
-    Type: ["Fire"]
-}
-]
-
-function add(pokemon){
-    if(
-        typeof pokemon === "object" &&
-        "Name" in pokemon &&
-        "Height" in pokemon &&
-        "Type" in pokemon
-    ){
-        pokemonList.push(pokemon)
-    }else{
-        console.log("Pokemon is not Correct")
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  
+    function add(item) {
+        if (
+          typeof item === "object" && 'name' in item && 'detailsUrl' in item
+        ) {
+          pokemonList.push(item);
+        } else {
+          console.log("Please check the inputs");
+        }
+      }
+    function getAll(){
+        return pokemonList;
     }
-}
-function getAll(){
-    return pokemonList;
-}
-return {
-    add: add,
-    getAll: getAll,
-}
-})()
+    
+    async function loadList() {
+        try {
+            const response = await fetch(apiUrl);
+            const json = await response.json();
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        } catch (e) {
+            console.error(e);
+        }
+      }
+      async function loadDetails(item) {
+        let url = item.detailsUrl;
+        try {
+              const response = await fetch(url);
+              const details = await response.json();
+              item.imageUrl = details.sprites.other["official-artwork"].front_default;
+              item.height = details.height;
+              item.types = [];
+              for (var i = 0; i < details.types.length; i++) {
+                  item.types.push(details.types[i].type.name);
+              }
+          } catch (e) {
+              console.error(e);
+          }
+      }
 
-console.log(pokemonRepository.getAll())
+      function showDetails(pokemon) {
+        loadDetails(pokemon).then(function () {
+          console.log(pokemon);
+        });
+      }
+      
+      function addListItem(pokemon){
+        let pokemonList = document.querySelector(".PokemonList")
+        let listpokemon = document.createElement("li");
+        let button = document.createElement("button")
+        button.innerText = pokemon.name;
+        button.classList.add("button-class")
+        listpokemon.appendChild(button)
+        pokemonList.appendChild(listpokemon)
+        button.addEventListener("click", function (){
+            showDetails(pokemon)
+        })
+      }
+      
 
-pokemonRepository.getAll().forEach(function(item){
-    let size;
-    if (item.Height > 1 ){
-        size = 'Wow, Thats a big Pokemon'
-    }else{
-        size = 'Dang, Thats a small Pokemon'
-    }
+  
+    return {
+      add: add,
+      getAll: getAll,
+      loadList: loadList,
+      addListItem: addListItem,
+      loadDetails: loadDetails,
+      showDetails: showDetails
+    };
+  })();
+  
+  pokemonRepository.loadList().then(function() {
+    // Now the data is loaded!
+    pokemonRepository.getAll().forEach(function(pokemon){
+      pokemonRepository.addListItem(pokemon);
+    });
+  });
 
-item.Type.forEach(function(typeItem){
-if (typeItem === "grass"){
-    result = '<span style="color: green"></span>'
-}else if(typeItem == "Fire"){
-    result = '<span style="color: red"></span>'
-}else if(typeItem == "Electric"){
-    result = '<span style="color: yellow"></span>'
-}else if(typeItem == "poison"){
-    result = '<span style="color: rgb(106,42,106)"></span>'
-}else if(typeItem == "psychic"){
-    result = '<span style="color: orange"></span>'
-}
-})
-
-document.write(
-    '<div class="box">' +
-    "Name" +
-    item.Name +
-    "<br>" +
-    "Height: " +
-    item.Height +
-    "m" + 
-    "<br>" +
-    "Type: "+
-    item.Type + 
-    "<br>"+
-    '</div>');
-
-})
 
